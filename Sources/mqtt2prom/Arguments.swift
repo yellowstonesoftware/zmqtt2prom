6ca64c8b40f6d20feb8eeac31419c0b05a2135b2
yellowstonesoftware <os@yellowstonesoftware.io>
2025-07-28 13:39:18 -0600

    GHA workflows and initial release

--- Sources/mqtt2prom/Arguments.swift
+++ Sources/mqtt2prom/Arguments.swift
@@ -14,7 +14,7 @@ struct ZMQTT2Prom: AsyncParsableCommand {
         subscribes to individual device topics, and transforms the data into Prometheus-compatible
         metrics based on the device expose schemas.
         """,
-        version: "1.0.0"
+        version: "0.0.1"
     )
     
     @Option(name: .long, help: "MQTT broker hostname")
