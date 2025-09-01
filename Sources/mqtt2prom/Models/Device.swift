b5db358ac7f85d1f36b6ee59aa00e3e8927d2d10
yellowstonesoftware <os@yellowstonesoftware.io>
2025-09-01 16:15:52 -0600

    reformat

--- Sources/mqtt2prom/Models/Device.swift
+++ Sources/mqtt2prom/Models/Device.swift
@@ -1,47 +1,47 @@
 import Foundation
 
 struct Device: Codable, Sendable {
-    let disabled: Bool
-    let friendlyName: String
-    let ieeeAddress: String
-    let interviewCompleted: Bool
-    let manufacturer: String?
-    let modelId: String?
-    let networkAddress: UInt16
-    let supported: Bool
-    let type: String
-    let definition: DeviceDefinition?
-    
-    private enum CodingKeys: String, CodingKey {
-        case disabled
-        case friendlyName = "friendly_name"
-        case ieeeAddress = "ieee_address"
-        case interviewCompleted = "interview_completed"
-        case manufacturer
-        case modelId = "model_id"
-        case networkAddress = "network_address"
-        case supported
-        case type
-        case definition
-    }
-    
-    /// Check if device is eligible for monitoring
-    var isEligible: Bool {
-        return supported && !disabled && interviewCompleted
-    }
-    
-    var mqttTopic: String {
-        return "zigbee2mqtt/\(friendlyName)"
-    }
+  let disabled: Bool
+  let friendlyName: String
+  let ieeeAddress: String
+  let interviewCompleted: Bool
+  let manufacturer: String?
+  let modelId: String?
+  let networkAddress: UInt16
+  let supported: Bool
+  let type: String
+  let definition: DeviceDefinition?
+
+  private enum CodingKeys: String, CodingKey {
+    case disabled
+    case friendlyName = "friendly_name"
+    case ieeeAddress = "ieee_address"
+    case interviewCompleted = "interview_completed"
+    case manufacturer
+    case modelId = "model_id"
+    case networkAddress = "network_address"
+    case supported
+    case type
+    case definition
+  }
+
+  /// Check if device is eligible for monitoring
+  var isEligible: Bool {
+    return supported && !disabled && interviewCompleted
+  }
+
+  var mqttTopic: String {
+    return "zigbee2mqtt/\(friendlyName)"
+  }
 }
 
 struct DeviceDefinition: Codable, Sendable {
-    let description: String?
-    let model: String?
-    let vendor: String?
-    let exposes: [Expose]
-    
-    private enum CodingKeys: String, CodingKey {
-        case description, model, vendor, exposes
-    }
-} 
+  let description: String?
+  let model: String?
+  let vendor: String?
+  let exposes: [Expose]
+
+  private enum CodingKeys: String, CodingKey {
+    case description, model, vendor, exposes
+  }
+}
--- Sources/mqtt2prom/Services/Controllers/MetricsController.swift
+++ Sources/mqtt2prom/Services/Controllers/MetricsController.swift
@@ -1,21 +1,21 @@
 import Foundation
 import Hummingbird
-import Prometheus
 import Logging
+import Prometheus
 
 struct MetricsController {
-    let registry: PrometheusCollectorRegistry
+  let registry: PrometheusCollectorRegistry
 
-    var endpoints: RouteCollection<BasicRequestContext> {
-        return RouteCollection(context: BasicRequestContext.self)
-            .get(use: self.list)
-    }
+  var endpoints: RouteCollection<BasicRequestContext> {
+    return RouteCollection(context: BasicRequestContext.self)
+      .get(use: self.list)
+  }
 
-    @Sendable func list(request: Request, context: some RequestContext) async throws -> String {
-        var buffer = [UInt8]()
-        buffer.reserveCapacity(20*1024) // TODO reasonable default?
-        registry.emit(into: &buffer)
+  @Sendable func list(request: Request, context: some RequestContext) async throws -> String {
+    var buffer = [UInt8]()
+    buffer.reserveCapacity(20 * 1024)  // TODO reasonable default?
+    registry.emit(into: &buffer)
 
-        return String(decoding: buffer, as: Unicode.UTF8.self)
-    }
+    return String(decoding: buffer, as: Unicode.UTF8.self)
+  }
 }
